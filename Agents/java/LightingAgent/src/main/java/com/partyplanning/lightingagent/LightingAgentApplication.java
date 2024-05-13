54caf5f35ebfa39823062bb313f379d092a170e7
Matthew Bolanos <matthewbolanos@gmail.com>
2024-05-12 17:24:48 -0700

    Setup Java project

---
+++ Agents/java/LightingAgent/src/main/java/com/partyplanning/lightingagent/LightingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.partyplanning.lightingagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightingAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightingAgentApplication.class, args);
+	}
+
+}
---
+++ Agents/java/LightingAgent/src/test/java/com/partyplanning/lightingagent/LightingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partyplanning.lightingagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightingAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
